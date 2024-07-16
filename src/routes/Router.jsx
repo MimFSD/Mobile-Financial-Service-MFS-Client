eb2817f7437c9f504bf8c39af69cc607612cbede
MimFSD <mimislam878@gmail.com>
2024-07-16 10:16:44 +0600

    logo and title setup

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,9 +1,24 @@
-import { createBrowserRouter } from "react-router-dom"
-import App from "../App"
+import { createBrowserRouter } from "react-router-dom";
+import Dashboard from "@/pages/Dashboard";
+import Login from "@/pages/shared/Login";
+import Register from "@/pages/shared/Register";
 
 export const router = createBrowserRouter([
     {
-        path: "/",
-        element: <App></App>,
+        path: "/login",
+        element: <Login />,
     },
-])
+    {
+        path: "/register",
+        element: <Register />,
+    },
+    {
+        path: "/",
+        element: <Dashboard></Dashboard>,
+        children: [
+            {
+                // path
+            },
+        ],
+    },
+]);
